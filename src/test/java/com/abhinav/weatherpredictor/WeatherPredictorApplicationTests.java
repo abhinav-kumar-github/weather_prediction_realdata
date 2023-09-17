c93ef4e44b8d2fa381f4c6ee2466ee7c66305028
Abhinav Kumar <abhinku7@cisco.com>
2023-09-17 21:36:30 +0530

    Initial commit

---
+++ src/test/java/com/abhinav/weatherpredictor/WeatherPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.weatherpredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
